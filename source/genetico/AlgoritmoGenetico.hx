1b4b14f94e5333b44bd3a8f68b4058fdcebae591
Paulo Roberto Cruz <paulo.cruz9@gmail.com>
2016-11-20 01:44:45 -0200

    implementada função cruar, ainda não funciona completamente correto

--- source/genetico/AlgoritmoGenetico.hx
+++ source/genetico/AlgoritmoGenetico.hx
@@ -2,16 +2,22 @@ package genetico;
 
 class AlgoritmoGenetico{
 
-    var taxaMutacao:Float = 0.035;
+    static var quantidadeDeRecursos:Int;
 
-    var tamanho_amostra_rota:Int = 10;
+    static var taxaMutacao:Float = 0.035;
 
-    var elitismo:Bool = true;
+    static var tamanho_amostra_rota:Int = 10;
+
+    static var elitismo:Bool = true;
 
     static public function evoluir(pop:Populacao):Populacao{
       
-      var novaPop:Populacao = new Populacao();
+      quantidadeDeRecursos = pop.getQuantidadeRecursos();
+      
+      trace("evolução");
 
+      var novaPop:Populacao = new Populacao(pop.getQuantidadeRecursos());
+      
       novaPop.iniciar();
       
       var offset:Int = 0;
@@ -29,7 +35,7 @@ class AlgoritmoGenetico{
 
         var filho:Individuo = cruzar(pai, mae);
 
-        filho = mutacao(filho);
+        filho = mutacao(filho);        
 
         novaPop.add(filho);
 
@@ -39,7 +45,47 @@ class AlgoritmoGenetico{
     }
 
     static public function cruzar(p:Individuo, m:Individuo):Individuo{
-      var mae:Individuo;
+      
+      var filho:Individuo = new Individuo();
+      
+      var quantidadePai:Int;
+      var quantidadeMae:Int; 
+
+      var rp:Array<Predio> = p.getRecursos();
+      var rm:Array<Predio> = m.getRecursos();
+      
+      trace("pai:"+rp.length +" mae:"+ rm.length);
+
+      quantidadePai = Math.ceil(rp.length/2);
+      quantidadeMae = Math.floor(rm.length/2);
+
+      //pai 
+      for(i in 0...quantidadePai){
+        var index:Int = Std.int( Math.random() * rp.length );
+        var predio:Predio = rp[index];
+        filho.setRecurso(predio.id, new Recurso());
+        rp.remove(predio);
+      }
+
+      //mae
+      for(i in 0...quantidadeMae){        
+        var ok:Bool = false;
+        var index:Int = -1;
+        //certifica que não vai sobreescrever um recurso ja existente no filho
+        while( ok == false){
+          trace("tentativa_"+i);
+          index = Std.int( Math.random() * rm.length );
+          if (filho.contemRecurso(index)  == false){
+            ok = true;
+          }
+        }
+        
+        var predio:Predio = rm[index];
+        filho.setRecurso(predio.id, new Recurso());
+        rm.remove(predio);
+      }
+      //não deixa repetir
+      return filho;
     }
 
     static public function mutacao(i:Individuo):Individuo{
@@ -49,6 +95,7 @@ class AlgoritmoGenetico{
     static private function seleciona(pop:Populacao):Individuo {
 
         var sl_pop:Populacao = new Populacao();
+        sl_pop.iniciar();
 
         for (i in 0...tamanho_amostra_rota) {
             //não deixar repetir? usar outro array para retirar possibilidades 
